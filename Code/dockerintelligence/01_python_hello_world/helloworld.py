eab80955e23905621d91010eb22f358f8ae8fc14
RAMKUMAR K <122148111+ramkumar-bitsathy@users.noreply.github.com>
2024-09-29 16:18:24 +0530

    Delete Code/dockerintelligence directory

--- Code/dockerintelligence/01_python_hello_world/helloworld.py
+++
@@ -1,8 +0,0 @@
-def main():
-    print("Hello world")
-    print("Ramkumar K")
-    print("7376222IT231")
-    print("Department of Information Techology.")
-    
-if __name__ == "__main__":
-    main()
